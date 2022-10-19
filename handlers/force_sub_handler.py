bc8e91baa148cccf9ad566b0bd3b3945571dc267
NANJIBLACK45 <102712986+NANJIBLACK45@users.noreply.github.com>
2022-10-19 10:19:00 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -32,7 +32,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/Quality_Linkzs).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/Quality_Links_TG).",
                 disable_web_page_preview=True
             )
             return 400
@@ -61,7 +61,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/Quality_Linkzs).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/Quality_Links_TG).",
             disable_web_page_preview=True
         )
         return 200
